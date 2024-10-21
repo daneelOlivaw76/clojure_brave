0a9794d146cc3ef024a0b22b1f86015c3f03de6f
Rui Reis <67687938+uc-rreis@users.noreply.github.com>
2024-10-21 18:01:17 +0100

    emacs commands

---
+++ emacs/emacs_commands.clj
@@ -0,0 +1,12 @@
+;; Emacs commands cheat sheet
+
+;; BUFFER OPERATIONS
+;; C-x b                -- list all open buffers
+;; C-x k                -- kill buffer
+
+
+
+;; FILE OPERATIONS
+;; C-x C-f              -- open/create file
+;; C-x C-s/Cmd + S      -- save file
+
